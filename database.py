9e43a90b83b23e739334a865b9e04da3c2d90f8a
Slava <slava@mail.ru>
2024-05-21 23:09:03 +0700

    чуть-чуть обновил

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from config import settings
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:12345@localhost:5432/dbtest04"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgresuser:U4bbcW0F2xCTKhDiiLgC9J4kTwv2owIy@dpg-cp6b5sol6cac738hr7jg-a:5432/dbtest04_82k1"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
